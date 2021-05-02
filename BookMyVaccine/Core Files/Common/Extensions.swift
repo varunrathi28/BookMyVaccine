5ae0ceed44f9429edf555522edc6887d96bffeb2
Varun Rathi <varunrathi28@users.noreply.github.com>
2021-05-02 21:12:17 +0530

    Add Integer extension for string rep

---
+++ BookMyVaccine/Core Files/Common/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  BookMyVaccine
+//
+//  Created by VR on 02/05/21.
+//
+
+import Foundation
+
+
+extension Int {
+    var string_rep : String {
+        return String(describing: self)
+    }
+}
